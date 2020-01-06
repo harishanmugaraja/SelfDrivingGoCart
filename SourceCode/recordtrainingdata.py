2141b246a06ebb0b4bff4e5c8eb472d08daec592
Tony Liang <tonyliang02@gmail.com>
2020-01-06 14:07:14 -0500

    switch from timer to 500 lidar data points

--- SourceCode/recordtrainingdata.py
+++ SourceCode/recordtrainingdata.py
@@ -9,10 +9,10 @@ import os
 import signal
 filename = "steeringdata.txt"
 #cmd = ['/Users/tliang/Desktop/Syslab/rplidar_sdk-master/sdk/output/Darwin/Release/ultra_simple',
-       '/dev/tty.SLAB_USBtoUART', '115200']
+       #'/dev/tty.SLAB_USBtoUART', '115200']
 cmd = ['/home/pi/Desktop/rplidar_sdk-master/sdk/output/Linux/Release/ultra_simple']
 
-
+k = 0
 
 ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
@@ -21,16 +21,22 @@ def main():
     f = open(filename, "w")
 
     while True:
+        global k
         read_ser = ser.readline()
         f.write("new data")
         f.write(str(read_ser) + "\n")
         #print(read_ser)
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-        startTime = time.process_time_ns()
-        while time.process_time_ns() - startTime < .25 * 10^9:
-            for line in process.stdout:
-                f.write(line)
-        os.killpg(os.getpgid(cmd.pid), signal.SIGTERM)#end process code from stack overflow
+        #startTime = time.process_time_ns()
+        #while time.process_time_ns() - startTime < .25 * 10^9:
+        for line in process.stdout:
+            f.write(line)
+            k+=1
+            if k == 500:
+                
+                k = 0
+                break
+        #os.killpg(os.getpgid(cmd.pid), signal.SIGTERM)#end process code from stack overflow
         
     f.close()
 
