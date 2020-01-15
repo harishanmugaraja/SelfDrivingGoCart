3a10f54d9efd0bd0d50ffdc4e3c1b1ed3ba1b715
Tony Liang <tonyliang02@gmail.com>
2020-01-15 09:10:54 -0500

    changed usb to acm for serial

--- SourceCode/recordtrainingdata.py
+++ SourceCode/recordtrainingdata.py
@@ -17,7 +17,7 @@ usbforlidar = "/dev/ttyUSB" + usbforlidarprompt
 cmd.append(usbforlidar)
 
 k = 0
-ser=serial.Serial("/dev/ttyUSB0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
 
 
