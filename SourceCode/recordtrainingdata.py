9c80aaef0313ec41fc983d69501a6833a8c7fa68
Tony Liang <tonyliang02@gmail.com>
2020-01-15 09:00:39 -0500

    prompt for usb for lidar

--- SourceCode/recordtrainingdata.py
+++ SourceCode/recordtrainingdata.py
@@ -11,16 +11,18 @@ filename = "steeringdata.txt"
 #cmd = ['/Users/tliang/Desktop/Syslab/rplidar_sdk-master/sdk/output/Darwin/Release/ultra_simple',
        #'/dev/tty.SLAB_USBtoUART', '115200']
 cmd = ['/home/pi/Desktop/rplidar_sdk-master/sdk/output/Linux/Release/ultra_simple']
+usbforlidarprompt = sys.argv[1]#this will be a number, usually 1 or 2
+usbforlidar = "/dev/ttyUSB" + usbforlidarprompt
+cmd.append(usbforlidar)
 
 k = 0
-
+ser=serial.Serial("/dev/ttyUSB0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+    ser.baudrate=9600
 
 
 def main():
     
-    usbport = sys.argv[1]
-    ser=serial.Serial("/dev/ttyUSB" + usbport,9600)  #change ACM number as found from ls /dev/tty/ACM*
-    ser.baudrate=9600
+    
     
     f = open(filename, "w")
 
