516cf069650d7bba0900210ae35e8211a035c8db
Tony Liang <tonyliang02@gmail.com>
2020-01-17 09:59:13 -0500

    prompt for lidar and arduino ports

--- SourceCode/recordtrainingdata.py
+++ SourceCode/recordtrainingdata.py
@@ -1,4 +1,4 @@
-#Tony Liang
+#Tony Liang and hari
 #Nov 20 2019
 #take in value written to writeMicroseconds() on the arduino and log it in a text file here.
 import sys
@@ -9,18 +9,17 @@ import time
 import os
 import signal
 filename = "steeringdata.txt"
+
+acmn = sys.argv[1] #port n for arduino usually 1 or 0
+usbn = sys.argv[2] #port n for lidar, usually 1 or 0
+
 #cmd = ['/Users/tliang/Desktop/Syslab/rplidar_sdk-master/sdk/output/Darwin/Release/ultra_simple',
        #'/dev/tty.SLAB_USBtoUART', '115200']
-cmd = ['/home/pi/Desktop/rplidar_sdk-master/sdk/output/Linux/Release/ultra_simple', "/dev/ttyUSB1"]
-#usbforlidarprompt = sys.argv[1]#this will be a number, usually 1 or 2
-#usbforserialprompt = sys.argv[2]
-#usbforlidar = "/dev/ttyUSB" + usbforlidarprompt
-#usbforserial = "/dev/ttyACM" + usbforserialprompt
-#print("lidar" + usbforlidar + "arduino" + usbforserial)
-#cmd.append(usbforlidar)
+cmd = ['/home/pi/Desktop/rplidar_sdk-master/sdk/output/Linux/Release/ultra_simple', "/dev/ttyUSB" + usbn]
+
 
 k = 0
-ser=serial.Serial("/dev/ttyACM0",9600)  #change ACM number as found from ls /dev/tty/ACM*
+ser=serial.Serial("/dev/ttyACM" + acmn,9600)  #change ACM number as found from ls /dev/tty/ACM*
 ser.baudrate=9600
 
 
